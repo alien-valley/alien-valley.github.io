6cb9cba29b006946ba9824d313ab6db8a0fa7e52
VONSBÄK <alien-valley-vonsbak@protonmail.com>
2021-12-03 10:23:04 +0100

    Add: who-to-delegate

---
+++ who-to-delegate/index.md
@@ -0,0 +1,36 @@
+---
+layout: default
+---
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+## The data is live and fed from [alien-valley.io](http://alien-valley.io/) pillar
+
+## Assumptions:
+- a delegation of 1000 ZNN
+- pillar momentums are scaled to fit the whole epoch (percentage of momentums produced remains the same)
+- - eg: if Pillar 1 produced 10% of momentums when the page loades, it will produce 10% of momentums at the end of the epoch
+
+## Known
+- total daily rewards of 14.400K ZNN in the beginning 
+- 8.640 expected momentums in a day
+- 50% of znn reward goes to momentum producing - 7.200
+- 24% of znn reward goes to momentum producing - 3.456
+
+<table id="myTable">
+    <thead>
+        <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Del %</th>
+            <th>Mom %</th>
+            <th>Weight</th>
+            <th>Stats</th>
+            <th>Returns</th>
+        </tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
+<script src="script.js"></script>
---
+++ who-to-delegate/script.js
@@ -0,0 +1,55 @@
+$.ajax({
+    type: 'POST',
+    url: 'http://139.177.178.226:35997',
+    crossDomain: true,
+    data: '{"jsonrpc": "2.0","id": 30,"method": "embedded.pillar.getAll","params": [0, 150]}',
+    beforeSend: function (xhr) {
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("Accept", "application/json, text/plain, */*");
+    }, success: function (data) {
+        var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+        let rewardPerProducedMomentum = 7200 / 8640;
+        let totalDelegateRewards = 3456;
+        let totalMomentumsInAnEpoch = 8640;
+
+        let totalWeight = 0;
+        let totalExpected = 0;
+        let delegatedZnn = 1000 * 100000000;
+
+        for (let index in data.result.list) {
+            val = data.result.list[index];
+            totalWeight += val.weight;
+            totalExpected += val.currentStats.expectedMomentums;
+        }
+
+        for (let index in data.result.list) {
+            val = data.result.list[index];
+
+            // Insert a row at the end of table
+            var newRow = tbodyRef.insertRow();
+            newRow.insertCell().appendChild(document.createTextNode(val.rank + 1));
+            newRow.insertCell().appendChild(document.createTextNode(val.name));
+            newRow.insertCell().appendChild(document.createTextNode(val.giveDelegateRewardPercentage));
+            newRow.insertCell().appendChild(document.createTextNode(val.giveMomentumRewardPercentage));
+
+            let w = (val.weight / 100000000000).toFixed(3);
+            newRow.insertCell().appendChild(document.createTextNode(w + "K"));
+
+            let stats = val.currentStats.producedMomentums + "/" + val.currentStats.expectedMomentums;
+            let percentageS = "0%";
+            let percentage = 0;
+            if (val.currentStats.expectedMomentums != 0) {
+                percentageS = (100 * val.currentStats.producedMomentums / val.currentStats.expectedMomentums).toFixed(0) + "%";
+                percentage = val.currentStats.producedMomentums / val.currentStats.expectedMomentums;
+            }
+            newRow.insertCell().appendChild(document.createTextNode(stats + " " + percentageS));
+
+            let returns = delegatedZnn / totalWeight * totalDelegateRewards * val.giveDelegateRewardPercentage / 100;
+            returns += totalMomentumsInAnEpoch / totalExpected * val.currentStats.producedMomentums * val.giveMomentumRewardPercentage / 100 * rewardPerProducedMomentum * delegatedZnn / (val.weight + delegatedZnn);
+            newRow.insertCell().appendChild(document.createTextNode(returns.toFixed(2)));
+        }
+
+        console.log(data);
+        //process the JSON data etc
+    }
+})
